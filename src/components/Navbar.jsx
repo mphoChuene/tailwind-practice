0ecd6011d099fe65ca86031fdf9d533fd08fe84e
mphoChuene <mphochuene42@gmail.com>
2023-07-29 18:14:31 +0200

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1 className="text-white">Whit3man Graphics.</h1>
+    </div>
+  );
+};
+
+export default Navbar;
