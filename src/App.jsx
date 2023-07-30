341fbf3a8f701922935538ed592d7063ed15fa51
mphoChuene <mphochuene42@gmail.com>
2023-07-30 18:23:02 +0200

    tailwind refresh issue unresolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from "react";
 import Navbar from "./components/navbar";
-import "./App.css";
+import "./index.css";
+// import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Navbar />
